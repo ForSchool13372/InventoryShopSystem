42888174af3f3547c0db45b97b3d17511d59eec2
Your Name <gobbler828@gmail.com>
2026-05-27 17:17:24 -0400

    fix

--- api.py
+++ api.py
@@ -21,7 +21,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://inventory-shop-system.vercel.app/"
+        "https://inventoryshopsystem.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
