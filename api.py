ba8cc0905c7031632eafa9f152f3ebf2426012a1
Your Name <gobbler828@gmail.com>
2026-05-27 01:00:00 -0400

    fix: update API URL for production

--- api.py
+++ api.py
@@ -17,10 +17,7 @@ gameFactory = GameFactory()
 #Security layer which websites can talk to my backend API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://inventoryshopsystem.vercel.app"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
